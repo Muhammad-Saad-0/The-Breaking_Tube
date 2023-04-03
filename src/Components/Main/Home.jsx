471abfd3f066570a688a7c047d973a572b655769
Muhammad-Saad-0 <121944357+Muhammad-Saad-0@users.noreply.github.com>
2023-04-04 01:50:46 +0500

    edit home

--- src/Components/Main/Home.jsx
+++ src/Components/Main/Home.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
-import HomeFilter from "./HomeFilter";
-import MainData from "../../Data/MainData";
 import "../../styles/Home.css";
-import NavBar from "../NavigationBar/NavBar";
-import SideBar from "../SideBar/SideBar";
 import VideosGrid from "./VideosGrid";
 import { Route, Routes } from "react-router";
 import Playlist from "./Playlist";
@@ -11,7 +7,6 @@ import VideoPage from "./VideoPage";
 import { useVideoId } from "../../context/videoIdContext";
 import { useFilterId } from "../../context/FilterContext";
 import { useSidebar } from "../../context/SideBarContext";
-import SignInPage from "./SignInPage";
 import Profile from "./Profile";
 import SignUp from "./SignUp";
 import SignIn from "./SignInPage";
