ad0c5059f786bcb424071407c12c3bff782808c9
Muhammad-Saad-0 <121944357+Muhammad-Saad-0@users.noreply.github.com>
2023-04-06 20:40:02 +0500

    fix theme on reload

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,24 +1,26 @@
-import  React , { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 const ThemeContext = React.createContext();
 const ThemeUpdateContext = React.createContext();
 
-export function useTheme(){
-    return useContext(ThemeContext)
+export function useTheme() {
+  return useContext(ThemeContext);
 }
-export function useThemeUpdate(){
-    return useContext(ThemeUpdateContext)
+export function useThemeUpdate() {
+  return useContext(ThemeUpdateContext);
 }
 export function ThemeProvider({ children }) {
-  const [Theme, setTheme] = useState(false);
+  const [Theme, setTheme] = useState(
+    localStorage.getItem("theme") === "true" ? true : false
+  );
 
   function toggleTheme() {
-    setTheme(prev => !prev);
-    localStorage.setItem('theme', Theme)
+    setTheme((prev) => !prev);
   }
-  useEffect(()=>{
-    setTheme(  localStorage.getItem('theme'))
-  },[])
+
+  useEffect(() => {
+    localStorage.setItem("theme", Theme);
+  }, [Theme]);
   return (
     <ThemeContext.Provider value={Theme}>
       <ThemeUpdateContext.Provider value={toggleTheme}>
